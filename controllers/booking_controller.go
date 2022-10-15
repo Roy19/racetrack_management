dc6cc191902e7c1102f52729c69074ed8bc79e9f
Aritra Sur Roy <aritra1997@hotmail.com>
2022-10-16 01:45:25 +0530

    add booking container

--- controllers/booking_controller.go
+++ controllers/booking_controller.go
@@ -1,1 +1,30 @@
 package controllers
+
+import (
+	"time"
+
+	"github.com/Roy19/racetrack-management/interfaces"
+	"github.com/Roy19/racetrack-management/models"
+)
+
+type BookingController struct {
+	bookingService interfaces.IBookingService
+}
+
+func (b *BookingController) BookSlot(vehicleType models.VehicleType,
+	identificationNumber string,
+	startTime time.Time) bool {
+	endTime := startTime.Add(time.Hour * time.Duration(3))
+	slotToBook := &models.BookedSlot{
+		Vehicle: &models.Vehicle{
+			VehicleType:          vehicleType,
+			IdentificationNumber: identificationNumber,
+		},
+		StartTime: startTime,
+		EndTime:   endTime,
+	}
+	// try booking a slot
+	ans := b.bookingService.TryBookingSlot(slotToBook)
+	// check if vehicle can be assigned
+	return ans
+}
